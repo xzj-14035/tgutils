b858d9d6684cfa64868a011dc99e26df1fd02050
Oren Ben-Kiki <oren@ben-kiki.org>
2019-07-04 16:38:18 +0300

    Hack for automating use of stubs in dependent packages.

---
+++ tgutils/setup_mypy.py
@@ -0,0 +1,10 @@
+"""
+Import this module in the ``setup.py`` file to use the provided Numpy/Pandas ``mypy`` stubs.
+
+TODO: This is a horrible hack.
+"""
+
+import os
+
+os.environ['MYPYPATH'] = \
+    os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), 'stubs')
