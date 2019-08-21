1aab9049eb7b38879867b8cadf4d830b82ce92ae
Oren Ben-Kiki <oren@ben-kiki.org>
2019-08-21 14:41:50 +0300

    Add more stubs.

---
+++ tgutils/poor_stubs/numpy/char.py
@@ -0,0 +1,10 @@
+"""
+Stubs for ``mypy`` for ``numpy.char``.
+"""
+
+from numpy import ndarray
+from typing import Any
+from typing import Optional
+
+
+def find(array: ndarray, string: str) -> ndarray: ...
